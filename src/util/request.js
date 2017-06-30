2cd30a3ce78e83db12a48de24fa6ada5cdd09513
Devmolasses <devmolasses@gmail.com>
2017-06-30 11:08:26 -0500

    Removed unnecessary code

--- src/util/request.js
+++ src/util/request.js
@@ -1,10 +1,5 @@
 const $ = require("jquery");
 
-// function requestJSON(channelName) {
-//   console.log($.getJSON(`https://dweet.io/get/latest/dweet/for/${channelName}`));
-//   return $.getJSON(`https://dweet.io/get/latest/dweet/for/${channelName}`);
-// }
-
 function requestJSON(channelName) {
   var jsonResponse = $.getJSON(`https://dweet.io/get/latest/dweet/for/${channelName}`);
   console.log(jsonResponse);
